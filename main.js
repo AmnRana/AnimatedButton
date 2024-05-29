18a6bd2acf0337b996b0a667465b4e8897b2ad1d
AmnRana <168154612+AmnRana@users.noreply.github.com>
2024-05-29 04:07:02 +0000

    sun and moon added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const mainButton= document.querySelector('.main-button')
 const sun= document.querySelector('.sun')
 mainButton.addEventListener('click',()=>{
-    
+    mainButton.classList.toggle('aman')
+    sun.classList.toggle('moon')
 })
