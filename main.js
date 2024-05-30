3e49ff607076ca5e0867dc21350dd85585d4927d
AmnRana <168154612+AmnRana@users.noreply.github.com>
2024-05-30 03:28:45 +0000

    dark-light mode

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const mainButton= document.querySelector('.main-button')
 const sun= document.querySelector('.sun')
+const earth=document.querySelector('.earth')
 mainButton.addEventListener('click',()=>{
     mainButton.classList.toggle('aman')
     sun.classList.toggle('moon')
+    earth.classList.toggle('main-container')
 })
